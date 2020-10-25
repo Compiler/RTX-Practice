f6a4823b37d71621111c734aadc76ae29f2b9775
compiler <lukerochedev@gmail.com>
2020-10-25 15:15:07 -0400

    moved to core

--- src/Core.h
+++ src/Core.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
 
 // Usings
@@ -19,6 +20,25 @@ constexpr double _PI_ = 3.1415926535897932385;
 inline double degreesToRadians(double degrees) {
     return degrees * _PI_ / 180.0;
 }
+
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
 
 // Common Headers
 #include <maths/Ray.h>
