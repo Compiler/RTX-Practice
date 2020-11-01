f168eae197b52b5b7bada73f8bf693e30dcfb1c6
compiler <lukerochedev@gmail.com>
2020-11-01 13:05:01 -0500

    aliasing

--- src/OtherLauncher/CoreLauncher.cpp
+++ src/OtherLauncher/CoreLauncher.cpp
@@ -16,10 +16,14 @@ double hitSphere(const Ray& currentRay, const Point3& sphereCenter, double spher
 }
 
 
-Color getRayColor(const Ray& currentRay, const RayHittableList& world){
-    
+Color getRayColor(const Ray& currentRay, const RayHittableList& world, int depth = 50){
+    if (depth <= 0)
+        return Color(0,0,0);
     RayHitRecord record;
-    if(world.hit(currentRay, 0, INFINITY, record)){
+    if(world.hit(currentRay, 0.001, INFINITY, record)){
+        Point3 newTarget = record.collisionPoint + record.collisionNormal + random_in_unit_sphere();
+        Ray newRay(record.collisionPoint, newTarget - record.collisionPoint);
+        return 0.5 * getRayColor(newRay, world, depth-1);
         return 0.5* (record.collisionNormal + Color(1));
     }
     auto t = 0.5*(unit_vector(currentRay.direction()).y() + 1.0);
@@ -42,7 +46,7 @@ void CoreLauncher::launch(const char* fileName){
     auto lowerLeftCorner = cameraRay.origin() - horizontal/2 - vertical/2 - Vec3(0, 0, focalLength);
 
     RayHittableList world;
-   // world.add(make_shared<SphereHittable>(Point3(0,-100.5,-1), 100.0));
+    world.add(make_shared<SphereHittable>(Point3(0,-100.5,-1), 100.0));
     world.add(make_shared<SphereHittable>(Point3(0,0,-1), 0.5));
 
 
@@ -55,16 +59,25 @@ void CoreLauncher::launch(const char* fileName){
     for(double y = IMAGE_HEIGHT - 1; y >= 0; y--){
         std::cerr << "\rScanlines remaing: " << y << " " << std::flush;
         for(double x = 0; x < IMAGE_WIDTH; x++){
-            double U = x / (IMAGE_WIDTH - 1);
-            double V = y / (IMAGE_HEIGHT - 1);
+            Color pixelColor;
+            for (int s = 0; s < 100; ++s) {
+                double U = (x + random_double()) / (IMAGE_WIDTH-1);
+                double V = (y + random_double()) / (IMAGE_HEIGHT-1);
+                currentRay.orig = cameraRay.origin();
+                currentRay.dir = lowerLeftCorner + U*horizontal + V*vertical - currentRay.orig;
+                currentRay.dir = currentRay.dir / currentRay.dir.length();
+
+                pixelColor += getRayColor(currentRay, world);
+            }
+            pixelColor /= 100.0;
+
             //std::cout << "\nU, V = (" << U << ", " << V << ")\n";
             
-            currentRay.orig = cameraRay.origin();
-            currentRay.dir = lowerLeftCorner + U*horizontal + V*vertical - currentRay.orig;
+            //currentRay.dir = lowerLeftCorner + U*horizontal + V*vertical - currentRay.orig;
             //std::cout << "\ncameraDir = (" << currentRay.dir.x() << ", " << currentRay.dir.y() << ", " << currentRay.dir.z() <<  ")\n";
-            currentRay.dir = currentRay.dir / currentRay.dir.length();
-            auto pixelColor = getRayColor(currentRay, world);
-            fprintf(file, "%d %d %d\n",(int)(pixelColor.e[0]* 255), (int)(pixelColor.e[1]*255), (int)(pixelColor.e[2]*255));
+            //currentRay.dir = currentRay.dir / currentRay.dir.length();
+            //pixelColor = getRayColor(currentRay, world);
+            fprintf(file, "%d %d %d\n",(int)(256 * clamp(pixelColor.e[0],0,0.999)),(int)(256 * clamp(pixelColor.e[1],0,0.999)),(int)(256 * clamp(pixelColor.e[2],0,0.999)));
             
         }
     }
